6fa12052ae8a88a2b94f9fb6ff948714f7847ba3
Anton_Kondratev <elkondro@gmail.com>
2023-02-17 13:48:51 +0700

    task_41 solution

--- task_41/Program.cs
+++ task_41/Program.cs
@@ -1,2 +1,12 @@
 ﻿Console.Clear();
 Console.Write("seminar_6 task_41\n\n");
+
+Console.Write("input numbers through space:\n");
+int[] numbers = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int count = 0;
+foreach (int num in numbers){
+    if (num > 0){
+        count += 1;
+    }
+}
+Console.Write($"-> {count}");

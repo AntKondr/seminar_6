d64e931aca87c17d3cfa6f0bc49a059d4f4cef64
Anton_Kondratev <elkondro@gmail.com>
2023-02-18 06:03:01 +0700

    task_add_1 solution

--- task_add_1/Program.cs
+++ task_add_1/Program.cs
@@ -1,2 +1,26 @@
 ﻿Console.Clear();
-Console.Write("seminar_6 task_add_1\n\n");
+Console.Write("seminar_6 task_add_1\n");
+Console.Write("permutations\n\n");
+
+void perm(char[] arr, int n){
+    if (n < 2){
+        Console.Write($"{string.Join("", arr)}\n");
+    }
+    else {
+        for (int j = n - 1; j >= 0; j--){
+            char temp = arr[j];
+            arr[j] = arr[n - 1];
+            arr[n - 1] = temp;
+            perm(arr, n - 1);
+            temp = arr[j];
+            arr[j] = arr[n - 1];
+            arr[n - 1] = temp;
+        }
+    }
+}
+
+Console.Write("input string:\n");
+char[] ch_arr = Console.ReadLine().ToCharArray();
+Console.Write("\n");
+
+perm(ch_arr, ch_arr.Length);
